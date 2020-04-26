f0827e04927a7aea629a0c27b039c93faf89d8bf
ghalynho10 <mghalynho@gmail.com>
2020-04-26 19:05:03 -0400

    latest changes to match the app name

--- core/management/commands/rename.py
+++ core/management/commands/rename.py
@@ -1,29 +1,33 @@
 import os
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     help = 'Renames a Django Projet'
 
     def add_arguments(self, parser):
-        parser.add_argument('new_project_name', type=str, help='The new Django project name')
+        parser.add_argument('new_project_name', type=str,
+                            help='The new Django project name')
 
     def handle(self, *args, **kwargs):
         new_project_name = kwargs['new_project_name']
 
-        #bit of logic to rename the project
+        # bit of logic to rename the project
 
-        files_to_rename = ['demo/settings/base.py', 'demo/wsgi.py', 'manage.py']
-        folder_to_rename = 'demo'
+        files_to_rename = ['myproject/settings/base.py',
+                           'myproject/wsgi.py', 'manage.py']
+        folder_to_rename = 'myproject'
 
         for f in files_to_rename:
             with open(f, 'r') as file:
                 filedata = file.read()
 
-            filedata = filedata.replace('demo', new_project_name)
+            filedata = filedata.replace('myproject', new_project_name)
 
-            with  open(f,'w') as file:
+            with open(f, 'w') as file:
                 file.write(filedata)
-            
+
         os.rename(folder_to_rename, new_project_name)
 
-        self.stdout.write(self.style.SUCCESS('Project has been renamed to %s' % new_project_name))
+        self.stdout.write(self.style.SUCCESS(
+            'Project has been renamed to %s' % new_project_name))
